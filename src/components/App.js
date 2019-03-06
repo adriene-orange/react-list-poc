69b83a5cebd3c51a5bc4a8c6f925c5dd077e74a2
Adriene <adriene_n_cranny@homedepot.com>
2019-03-06 08:25:45 -0800

    remove unused import

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import List from './list';
 import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware } from "redux";
